bc03776fe45f2e680a031f1492bb3979362ca9dc
Yaroslav <hawkbeat@mail.ru>
2022-10-22 16:53:07 +0600

    1 попытка проверки

--- Yatube/posts/views.py
+++ Yatube/posts/views.py
@@ -1,12 +1,12 @@
 # from multiprocessing import context
 # from urllib import request
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 # Create your views here.
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.order_by('-pub_date')[:10] 
     context = {
         'posts': posts,
     }
@@ -20,13 +20,14 @@ def index(request):
     # return render(request, template, context)
 
 
-def group_posts(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
+        'group': group,
         'posts': posts,
     }
     return render(request, 'posts/group_list.html', context)
-
 
     # template = 'posts/group_list.html'
     # title = 'Здесь будет информация о группах проекта Yatube'
